78a8581cfdcabacd68eb768445624e459f342c0e
R Balasubramaniam <145968893+UI-Bala@users.noreply.github.com>
2023-12-15 09:19:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let Resume={
+    "about": {
+      "name": "R Balasubramaniam",
+      "email": "ui.bala@yahoo.com",
+      "mobile": 8056406804,
+      "degree": "B.E",
+      "location": {
+        "address-line-1": "886-A Gandhi Nagar Second Street",
+        "address-line-2":"KR Palayam Bhavani",
+        "pinCode": 638301,
+        "city": "Erode",
+        "state": "Tamilnadu",
+        "country": "India"
+      }
+      
+    },
+
+
+    "work": [
+      {
+        "company": "Santhila DataBot Private Limited",
+        "position": "UI Developer"
+      }
+    ],
+
+
+    "education": [
+      {
+        "institution": "KSR Institute for Engineering and Technology",
+        "department": "Computer Science and Engineering",
+        "batch": 2015-2019,
+        "gpa": 7
+      }
+    ],
+
+
+    "skills": [
+      {
+        "name": "HTML, CSS, Bootstrap, JavaScript",
+        "level": "Intermediate"
+        
+      }
+    ],
+
+
+    "known-language": [
+      {
+        "language": "Tamil,English"
+      }
+    ],
+
+
+    "interests": [
+      {
+        "name": "Reading Books, Writing Tales"
+      }
+    ]
+  }
+  console.log(Resume);
